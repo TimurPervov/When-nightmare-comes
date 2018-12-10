f8c2cce3ca2a613b37dbb317e8976c151dd44729
Nosuicide <allakhovna@inbox.ru>
2018-12-11 01:25:17 +0300

    Смена дня и ночи
    Праект

---
+++ New Unity Project/Assets/GameTime.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTime : MonoBehaviour {
+    public float GameSeconds;
+    public float GameMinutes;
+
+    string stringSecond;
+    string stringMinutes;
+
+    public Text TextTime;
+
+	
+	void Update () {
+        GameSeconds = GameSeconds + Time.deltaTime;
+        stringSecond = GameSeconds.ToString();
+        stringMinutes = GameMinutes.ToString();
+
+        TextTime.text = "Время -" + stringMinutes + ":" + stringSecond;
+        
+        if(GameSeconds >=60.0f)
+        {
+            GameMinutes = GameMinutes + 1.0f;
+            GameSeconds = 0.0f;
+          }
+        if(GameMinutes>=24.0f)
+        {
+            GameMinutes = 0.0f;
+        }
+	}
+}
---
+++ New Unity Project/Assets/SunLuna.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SunLuna : MonoBehaviour {
+
+    float hours = 5;
+    float minutes = 45;
+    public float Speed;
+
+    public Text TextHours;
+    public Text TextMinutes;
+    public GameObject Centre;
+    
+	void FixedUpdate () {
+        Centre.transform.Rotate(Vector3.right * Speed * Time.deltaTime);
+        minutes = minutes + Speed + Time.deltaTime;
+        if(minutes>=60)
+        {
+            hours = hours + 1;
+            if(hours>=24)
+                            hours=0;
+            minutes = 0;
+        }
+        TextHours.text = hours.ToString();
+        TextMinutes.text = minutes.ToString();
+	}
+}
