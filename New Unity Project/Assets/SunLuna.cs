f5a0cd0696fa3116cb2e87a4b59ced7eed26f473
Andrey Ivanov <6june120@mail.ru>
2018-12-11 05:45:30 +0300

    Погода и движение персонажа
    Доработана система смены дня и ночи. Добавлен персонаж, управляемый игроком (стрелочками)

--- New Unity Project/Assets/SunLuna.cs
+++ New Unity Project/Assets/SunLuna.cs
@@ -1,29 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SunLuna : MonoBehaviour {
 
-    float hours = 5;
-    float minutes = 45;
-    public float Speed;
+        private float Speed=15.0f; //ОТНОШЕНИЕ 1 ГРАДУСА К ОДНОМУ TMIE = 1/4 БЛЯТЬ
+    public Text TextTime;
+    public GameObject Centre; 
+public GameObject Stars;
 
-    public Text TextHours;
-    public Text TextMinutes;
-    public GameObject Centre;
+int hours=0;
+float minutes=0;
+float tmie=360;
+
+private const float CheckPeriod = 1f;
+private float m_LastCheck = CheckPeriod;
+private int m_speed = 100;
+
+float a=0;
+
+void Start()
+{
+	
+}
     
-	void FixedUpdate () {
-        Centre.transform.Rotate(Vector3.right * Speed * Time.deltaTime);
-        minutes = minutes + Speed + Time.deltaTime;
-        if(minutes>=60)
-        {
-            hours = hours + 1;
-            if(hours>=24)
-                            hours=0;
-            minutes = 0;
+	
+void FixedUpdate () {
+        
+m_LastCheck = m_LastCheck -Time.deltaTime;
+if (m_LastCheck<0)
+{
+
+Centre.transform.Rotate(-1*Vector3.right*Speed);
+       tmie=tmie+60;
+       if (tmie >= 1440)
+	tmie = 0;
+	
+if (tmie>360 && tmie<1080)
+	Stars.SetActive(false);
+else Stars.SetActive(true);
+
+m_LastCheck=CheckPeriod;
+}
+
+      minutes = tmie%60;
+      hours = (int) Math.Truncate(tmie/60);
+
+//TextTime.text = a.ToString();
+TextTime.text = hours.ToString() + " часов " +minutes.ToString() + " минут ";
+
         }
-        TextHours.text = hours.ToString();
-        TextMinutes.text = minutes.ToString();
-	}
+        
 }
---
+++ New Unity Project/Assets/Supercyan Character Pack Free Sample/MaleMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaleMovement : MonoBehaviour {
+
+	public float speedMove;
+	private Vector3 moveVector;
+	
+	private CharacterController chcon;
+	private Animator animat;
+
+	private void Start()
+{
+	chcon = GetComponent<CharacterController>();
+	animat = GetComponent<Animator>();
+	
+}
+
+private void Update()
+{
+	CharacterMove();
+}
+
+private void CharacterMove()
+{
+	moveVector = Vector3.zero;
+	moveVector.x = Input.GetAxis("Horizontal")*speedMove;
+	moveVector.z = Input.GetAxis("Vertical")*speedMove;
+
+
+	if (moveVector.x!=0 || moveVector.z!=0) animat.SetBool("MoveBool",true);
+else animat.SetBool("MoveBool",false);
+
+	if (Vector3.Angle(Vector3.forward, moveVector)>1.0f || Vector3.Angle(Vector3.forward, moveVector) == 0)
+{
+	Vector3 direct = Vector3.RotateTowards(transform.forward,moveVector,speedMove,0.0f);
+transform.rotation = Quaternion.LookRotation(direct);
+}
+ 
+chcon.Move(moveVector*Time.deltaTime);
+}
+}
